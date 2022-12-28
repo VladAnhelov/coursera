aec7edcecdc70a511340a6f515e63b2926bb88bf
Vlad <vladanhelov@gmail.com>
2022-12-28 11:18:17 +0200

    add game

---
+++ js/components.js
@@ -0,0 +1,20 @@
+/////// LOAD IMAGES ////////
+
+// LOAD BG IMAGE
+const BG_IMG = new Image();
+BG_IMG.src = 'images/bibs.jpg';
+
+const LEVEL_IMG = new Image();
+LEVEL_IMG.src = 'images/level.png';
+
+const LIFE_IMG = new Image();
+LIFE_IMG.src = 'images/life.png';
+
+const SCORE_IMG = new Image();
+SCORE_IMG.src = 'images/score.png';
+/*
+const BONES_BRICKS_IMG = new Image();
+BONES_BRICKS.src = 'images/bonesBrick.png';
+*/
+
+/////// END LOAD IMAGES ////////
--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 let button = document.querySelectorAll('.button');
+// click add button to make a order
+
 for (let i = 0; i < button.length; i++)
   button[i].addEventListener('click', function () {
     button[i].textContent = 'Added';
