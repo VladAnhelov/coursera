aa3b0898cf8b6d745fc8353f7242225f4f86fe5e
Vlad <vladanhelov@gmail.com>
2023-01-04 17:35:39 +0200

    upgrade animation mobile menu icon

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ for (let i = 0; i < button.length; i++)
     button[i].textContent = 'Added';
     console.log(button[i], typeof button[i]);
   });
+
+function toggleMobileMenu(menu) {
+  menu.classList.toggle('open');
+}
+
+document.querySelectorAll('.menu').forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    btn.classList.toggle('active');
+  });
+});
